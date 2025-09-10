fcde15923badf8be1ed7d416fd2741c9f7b8d552
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-10 16:25:02 +0200

    Added new env approach

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -1,102 +1,71 @@
 import dotenv from 'dotenv'
 
-type ENV = { 
-    NEXT_PUBLIC_API_URL: string
-    DOCTL_TOKEN: string
-    PRIVATE_TOKEN: string
-    DB: string
-    DB_HOST: string
-    DB_USER: string
-    DB_PASSWORD: string
-    DB_PORT: string
-    DB_MAX_CONN: string
-    DB_IDLE_TIMEOUT_MS: string
-    DB_TIMEOUT_MS: string
-    CLIENT_ID: string
-    CLIENT_SECRET: string
-    REDIRECT_URI: string
-    BASE_URL: string
-    BEEKEEPER_URL: string
-    AUTHENTIK_TOKEN: string
-    DEFAULT_RESULTS_PER_PAGE: string
-    BTG_TOKEN: string
-    CRITICAL_ROLE: string
-    WEBHOOK_URL: string
-}
-
 dotenv.config({path: '../.env'})
 
-const {
-    DB_PORT,
-    DB_MAX_CONN,
-    DB_IDLE_TIMEOUT_MS,
-    DB_TIMEOUT_MS,
-    NEXT_PUBLIC_API_URL,
-    DOCTL_TOKEN,
-    PRIVATE_TOKEN,
-    DB,
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI,
-    BASE_URL,
-    BEEKEEPER_URL,
-    AUTHENTIK_TOKEN,
-    DEFAULT_RESULTS_PER_PAGE,
-    BTG_TOKEN,
-    CRITICAL_ROLE,
-    WEBHOOK_URL
-} = process.env as unknown as ENV
-if (!DOCTL_TOKEN
-    || !PRIVATE_TOKEN
-    || !DB
-    || !DB_HOST
-    || !DB_USER
-    || !DB_PASSWORD
-    || !CLIENT_ID
-    || !CLIENT_SECRET
-    || !REDIRECT_URI
-    || !BASE_URL
-    || !BEEKEEPER_URL
-    || !AUTHENTIK_TOKEN
-    || !BTG_TOKEN
-    || !WEBHOOK_URL
-) {
-    throw new Error("Missing one or more environment variables.")
+const requiredEnvironmentVariables = [
+    'DOCTL_TOKEN',
+    'PRIVATE_TOKEN',
+    'DB',
+    'DB_HOST',
+    'DB_USER',
+    'DB_PASSWORD',
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'REDIRECT_URI',
+    'BASE_URL',
+    'BEEKEEPER_URL',
+    'AUTHENTIK_TOKEN',
+    'BTG_TOKEN',
+    'WEBHOOK_URL',
+]
+
+const missingVariables = requiredEnvironmentVariables.filter(
+    (key) => !process.env[key]
+)
+
+if ( missingVariables.length > 0) {
+    throw new Error(
+        'Missing essential environment variables:\n' +
+            missingVariables
+                .map((key) => `${key}: ${process.env[key] || 'undefined'}`)
+                .join('\n')
+    )
 }
 
-const AUTH_URL = `${BASE_URL}/application/o/authorize/`
-const TOKEN_URL = `${BASE_URL}/application/o/token/`
-const USERINFO_URL = `${BASE_URL}/application/o/userinfo/`
-const USER_ENDPOINT = `${BASE_URL}/api/v3/core/users/`
+const env = Object.fromEntries(
+    requiredEnvironmentVariables.map((key) => [key, process.env[key]])
+)
+
+const AUTH_URL = `${env.BASE_URL}/application/o/authorize/`
+const TOKEN_URL = `${env.BASE_URL}/application/o/token/`
+const USERINFO_URL = `${env.BASE_URL}/application/o/userinfo/`
+const USER_ENDPOINT = `${env.BASE_URL}/api/v3/core/users/`
 
 const config = {
-    DB_PORT,
-    DB_MAX_CONN,
-    DB_IDLE_TIMEOUT_MS,
-    DB_TIMEOUT_MS,
-    NEXT_PUBLIC_API_URL,
-    DOCTL_TOKEN,
-    PRIVATE_TOKEN,
-    DB,
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI,
+    DB_PORT: env.DB_PORT,
+    DB_MAX_CONN: env.DB_MAX_CONN,
+    DB_IDLE_TIMEOUT_MS: env.DB_IDLE_TIMEOUT_MS,
+    DB_TIMEOUT_MS: env.DB_TIMEOUT_MS,
+    NEXT_PUBLIC_API_URL: env.NEXT_PUBLIC_API_URL,
+    DOCTL_TOKEN: env.DOCTL_TOKEN,
+    PRIVATE_TOKEN: env.PRIVATE_TOKEN,
+    DB: env.DB,
+    DB_HOST: env.DB_HOST,
+    DB_USER: env.DB_USER,
+    DB_PASSWORD: env.DB_PASSWORD,
+    CLIENT_ID: env.CLIENT_ID,
+    CLIENT_SECRET: env.CLIENT_SECRET,
+    REDIRECT_URI: env.REDIRECT_URI,
     AUTH_URL,
     TOKEN_URL,
     USERINFO_URL,
-    BEEKEEPER_URL,
+    BEEKEEPER_URL: env.BEEKEEPER_URL,
     USER_ENDPOINT,
-    AUTHENTIK_TOKEN,
-    BTG_TOKEN,
-    CRITICAL_ROLE,
-    WEBHOOK_URL,
-    DEFAULT_RESULTS_PER_PAGE: Number(DEFAULT_RESULTS_PER_PAGE) || 50
+    AUTHENTIK_TOKEN: env.AUTHENTIK_TOKEN,
+    BTG_TOKEN: env.BTG_TOKEN,
+    CRITICAL_ROLE: env.CRITICAL_ROLE,
+    WEBHOOK_URL: env.WEBHOOK_URL,
+    DEFAULT_RESULTS_PER_PAGE: Number(env.DEFAULT_RESULTS_PER_PAGE) || 50
 }
 
 export default config
